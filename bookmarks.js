eadfa8a0a8fb6427e2841d56a9fed368a69d1060
Gregory McNutt <amcnutt1996@gmail.com>
2026-07-27 15:30:10 -0400

    fixed bookmark browsing logic finally

--- bookmarks.js
+++ bookmarks.js
@@ -1,50 +1,120 @@
-async function fetchBookmarks() {
-	const root = await chrome.bookmarks.getTree();
-	const bookmarksBar = root[0].children[0].children;
-	return bookmarksBar;
+let bookMarkCache;
+let bookMarkStack = [];
+
+function getFavicon(iconURL) {
+    const url = new URL(chrome.runtime.getURL("/_favicon/"));
+    url.searchParams.set("pageUrl", iconURL);
+    url.searchParams.set("size", "32");
+    return url.toString();
+}
+
+function createBookMarkObjects(parentArray) {
+    // console.log(parentArray.children);
+
+    let bookmarkObjects = [];
+    let parentsChildren = parentArray.children;
+    // grab the children of each child array so we can get their data, or children if they have children.
+    for (let i = 0; i <= parentsChildren.length - 1; i++) {
+        // console.log(parentsChildren[i]);
+        if (parentsChildren[i].children) {
+            // console.log("has children");
+            // currentFoldersToDisplay.push(parentsChildren[i]);
+            //children == WILL BE A FOLDER FOR IMAGE.
+            let btn = document.createElement("button");
+
+            btn.addEventListener("click", () => {
+                let cont = document.getElementById("bookmarks-container");
+                handleFolderOpen(parentsChildren[i], cont);
+            });
+
+            let btnImg = document.createElement("img");
+            btnImg.src = "./folder.png";
+            btnImg.width = "50px";
+            btnImg.height = "50px";
+
+            let btnLabel = document.createElement("label");
+            btnLabel.innerText = parentsChildren[i].title;
+
+            btn.appendChild(btnImg);
+            btn.appendChild(btnLabel);
+
+            bookmarkObjects.push(btn);
+        } else {
+            // console.log("doesnt have children");
+            // currentBookMarksToDisplay.push(parentsChildren[i]);
+            let bmLink = document.createElement("a");
+            bmLink.href = parentsChildren[i].url;
+
+            let bmImage = document.createElement("img");
+            bmImage.src = getFavicon(parentsChildren[i].url);
+            bmImage.width = "50px";
+            bmImage.height = "50px";
+
+            let bmLabel = document.createElement("label");
+            bmLabel.innerText = parentsChildren[i].title;
+
+            bmLink.appendChild(bmImage);
+            bmLink.appendChild(bmLabel);
+
+            bookmarkObjects.push(bmLink);
+        }
+    }
+    return bookmarkObjects;
+    // return [currentBookMarksToDisplay, currentFoldersToDisplay];
+}
+
+function handleFolderOpen(parentArray, bookmarksContainer, backHidden) {
+    bookMarkStack.push(parentArray.parentId);
+    console.log(bookMarkStack);
+    bookmarksContainer.replaceChildren();
+
+    if (backHidden === true) {
+        console.log("back button hidden");
+    } else {
+        const backBtn = document.createElement("button");
+        backBtn.innerHTML = "<label>&larr;</label>";
+        backBtn.addEventListener("click", () => {
+            handleBackButton(parentArray, bookmarksContainer);
+        });
+
+        bookmarksContainer.appendChild(backBtn);
+    }
+    // console.log(parentArray[0].parentId); //this is to go up a folder -- save for later
+    console.log(parentArray);
+    let bookmarksCreatedObjects = createBookMarkObjects(parentArray);
+    buildSection(bookmarksContainer, bookmarksCreatedObjects);
+}
+
+function handleBackButton(parentArray, bookmarksContainer) {
+    console.log("back button pressed");
+    bookMarkStack.pop(-1);
+    console.log(bookMarkStack);
+    console.log(bookMarkStack[-1]);
+    bookmarksContainer.replaceChildren();
+    buildSection(
+        bookmarksContainer,
+        createBookMarkObjects(bookMarkCache[bookMarkStack.length - 1]),
+    );
+}
+
+function buildSection(bookmarksContainer, bookmarksAdding) {
+    for (let i = 0; i <= bookmarksAdding.length - 1; i++) {
+        // console.log(bookmarksAdding[i]);
+        bookmarksContainer.appendChild(bookmarksAdding[i]);
+    }
 }
 
 export const addBookmarksToPage = async () => {
-	const loadedBookmarksBar = await fetchBookmarks();
-	console.log(loadedBookmarksBar);
-	const bookmarksContainer = document.getElementById("bookmarks-container");
-	//TODO: add the images for the icons for each bookmark somehow
-	for (let i = 0; i < loadedBookmarksBar.length; i++) {
-		const bmarkLink = document.createElement("a");
-		//const bmarkImage = document.createElement("img");
-		const bmarkDiv = document.createElement("div");
-		const bmarkLabel = document.createElement("label");
+    const fetchBookmarks = await chrome.bookmarks.getTree();
+    bookMarkCache = fetchBookmarks[0].children;
+    // console.log(bookMarkCache); //gives back the whole data struct at it's [0] index since that's the base you can call .children from.
+    const baseBookMarksBar = bookMarkCache[0];
+    // console.log(baseBookMarksBar);
 
-		bookmarksContainer.appendChild(bmarkLink);
-		bmarkLink.appendChild(bmarkDiv);
-		bmarkDiv.appendChild(bmarkLabel);
+    const bookmarksContainer = document.getElementById("bookmarks-container");
+    handleFolderOpen(baseBookMarksBar, bookmarksContainer, true);
+    // buildSection(bookmarksContainer, bookmarkCreatedObjects);
 
-		bmarkDiv.setAttribute("class", "bookmark-div");
-		bmarkLink.setAttribute("href", loadedBookmarksBar[i].url);
-		bmarkLabel.innerText = loadedBookmarksBar[i].title;
-	}
+    // let [currentBookmarks, currentFolders] = getBookMarks(baseBookMarksBar);
+    // buildSection(bookmarksContainer, currentBookmarks, currentFolders);
 };
-
-// chrome.bookmarks.getTree((tree) => {
-// 	const bookmarkList = document.getElementById("bookmarkList");
-// 	displayBookmarks(tree[0].children, bookmarkList);
-// });
-
-// // Recursively display the bookmarks
-// function displayBookmarks(nodes, parentNode) {
-// 	for (const node of nodes) {
-// 		// If the node is a bookmark, create a list item and append it to the parent node
-// 		if (node.url) {
-// 			const listItem = document.createElement("li");
-// 			listItem.textContent = node.title;
-// 			parentNode.appendChild(listItem);
-// 		}
-
-// 		// If the node has children, recursively display them
-// 		if (node.children) {
-// 			const sublist = document.createElement("ul");
-// 			parentNode.appendChild(sublist);
-// 			displayBookmarks(node.children, sublist);
-// 		}
-// 	}
-// }
